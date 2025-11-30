a2f167a47d68d6d7462dc5618be6df2b9befc740
nanoenjoyer <ibrahimaxmed2002@gmail.com>
2025-11-30 21:07:31 +0300

    rename

--- move-script.sh
+++ move-script.sh
@@ -1,34 +1,51 @@
 #!/bin/bash
 
-if [ -f ~/downloads/*.jpg ]
+shopt -s nullglob
+
+# Create an array of image file paths
+images=(~/downloads/*.{jpg,jpeg,png,gif})
+
+# Check if the array is empty using the array count (#)
+if [ ${#images[@]} -eq 0 ]
 then
-    for file in ~/downloads/*.jpg
+    echo 'no pictures in downloads/'
+else
+    # Loop over the contents of the array, using quotes for safety
+    for file in "${images[@]}"
     do
       mv "$file" ~/pictures/
+      # Use $() for command substitution; $(basename "$file")
       echo $(basename "$file") has been moved to pictures/
     done
-else
-    echo "no .jpg files in downloads/"
 fi
 
-if [ -f ~/downloads/*.svg ]
+
+mp4Files=(~/downloads/*.mp4)
+
+# Check if the array is empty using the array count (#)
+if [ ${#mp4Files[@]} -eq 0 ]
 then
-    for file in ~/downloads/*.svg
+    echo "no mp4 files in downloads/"
+else
+    # Loop over the contents of the array, using quotes for safety
+    for file in "${mp4Files[@]}"
+    do
+      mv "$file" ~/videos
+      echo $(basename "$file") has been moved to videos/
+    done
+fi
+
+
+svg=(~/downloads/*.svg)
+
+if [ ${#svg[@]} -eq 0 ]
+then
+    echo "no svg files in downloads"
+else
+    # Loop over the contents of the array, using quotes for safety
+    for file in ${svg[@]}
     do
       mv "$file" ~/pictures/icons/
       echo $(basename "$file") has been moved to pictures/icons/
     done
-else
-    echo "no .svg files in downloads/"
-fi
-
-if [ -f ~/downloads/*.mp4 ]
-then
-    for file in ~/downloads/*.mp4
-    do
-      mv "$file" ~/videos/
-      echo $(basename "$file") has been moved to videos/
-    done
-else
-    echo "no .mp4 files in downloads/"
 fi
